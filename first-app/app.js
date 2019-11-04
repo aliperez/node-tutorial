c69de375ccabb1ffd4d5f8b30c88e2b560347c59
ALICIA <alicia.perez@utexas.edu>
2019-11-04 09:46:03 -0800

    tried exercise

--- first-app/app.js
+++ first-app/app.js
@@ -1,10 +1,29 @@
 
-const fs = require('fs');
+// Convention first letter of every word uppercase bc it's a class
+// Class is a container for properties and functions (methods)
+const EventEmitter = require('events');
 
-// const files = fs.readdirSync('./');
-// console.log(files);
+const logger = require('./logger');
 
-fs.readdir('$', function(err, files){
-    if (err) console.log('Error', err);
-    else console.log('Result', files);
-});
+// this emitter is an object
+// example: human is a class, John is an object
+const emitter = new EventEmitter();
+
+// Order matters, must emit after listener 
+
+// Register a listener
+// addListener same as on
+// emitter.on('messageLogged', (arg) => { //e, eventArg
+//     console.log('Listener called', arg);
+// })
+
+// Raised an event
+// object is referred to as the event argument
+// emitter.emit('messageLogged', { id: 1, url: 'http://'});
+
+// lisener for emitter in logger.js
+emitter.on('messageLogged', (arg) => { //e, eventArg
+    console.log('Listener called', arg);
+})
+
+
--- first-app/logger.js
+++ first-app/logger.js
@@ -1,7 +1,11 @@
+const EventEmit = require('events');
 
-var url = 'some random url';
+const emit = new EventEmit();
+// var url = 'some random url';
 
 function log(message) {
+    emit.emit('logging', { data: "my message here"});
+
     // Send an HTTP request
     console.log(message);
 }
