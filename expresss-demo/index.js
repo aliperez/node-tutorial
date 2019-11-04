e9fbb93dcb28e5bdc7cdbfc9130468adbbbb0f1e
ALICIA <alicia.perez@utexas.edu>
2019-11-04 14:18:52 -0800

    using route params

--- expresss-demo/index.js
+++ expresss-demo/index.js
@@ -9,13 +9,30 @@ const app = express();
 // app.put();
 // app.delete();
 
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'}
+];
+
 app.get('/', (req, res) => {
     res.send('Hello World!!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 })
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find( c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given Id was not found.');
+    res.send(course);
+})
+
+// app.get('/api/posts/:year/:month', (req, res) => {
+//     // res.send(req.params);
+//     res.send(req.query);
+// })
 
 // I used cli command "explort PORT=5000" to set the env variable
 const port = process.env.PORT || 3000;
