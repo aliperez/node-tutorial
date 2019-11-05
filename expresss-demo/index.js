8e3e9807e7b1079bde7c3a65519183defd69b3f1
ALICIA <alicia.perez@utexas.edu>
2019-11-05 09:07:34 -0800

    create read and update using express

--- expresss-demo/index.js
+++ expresss-demo/index.js
@@ -1,8 +1,14 @@
 
+// returns a class, so uppercase first letter
+const Joi = require('joi');
+
 // require express returns a function
 const express = require('express');
 // store the function as app, which is an object
 const app = express();
+
+// adding middleware
+app.use(express.json());
 
 // app.get();
 // app.post();
@@ -23,12 +29,59 @@ app.get('/api/courses', (req, res) => {
     res.send(courses);
 })
 
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    } 
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+})
+
+// route parameters
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find( c => c.id === parseInt(req.params.id));
     if (!course) res.status(404).send('The course with the given Id was not found.');
     res.send(course);
 })
 
+app.put('/api/courses/:id', (req, res)=> {
+    // Lookup the course by id
+    // If not existing, return 404 
+    const course = courses.find( c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given Id was not found.');
+
+    // Validate the course
+    // If invalid, return 400 - Bad Request
+    // const result = validateCourse(req.body);
+    // object destructuring on next line
+    const { error } = validateCourse(req.body); //result.error
+    
+    if (error) {
+        // 400 Bad Request
+        res.status(400).send(error.details[0].message);
+        return;
+    } 
+
+    // Update course
+    course.name = req.body.name;
+    // Return the updated course
+    res.send(course);
+})
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema);
+}
+
+// query string parameters are optional, put at end in url: ?sortBy=name
 // app.get('/api/posts/:year/:month', (req, res) => {
 //     // res.send(req.params);
 //     res.send(req.query);
