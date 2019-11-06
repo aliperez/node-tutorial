b3c7485e9dd22da8ef85d1191ce784a9e0d39110
ALICIA <alicia.perez@utexas.edu>
2019-11-06 14:37:55 -0800

    named functions to the rescue, resolving callback hell or christmas tree problem

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,20 @@
 console.log('Before');
-getUser(1, function(user) {
-    // console.log('User', user)
-    getRepositories(user.gitHubUsername, function(repos) {
-        console.log('Repositories: ', repos);
-    } )
-});
+getUser(1, getRepos);
 console.log('After');
+
+function getRepos(user) {
+    console.log('User found: ', user.gitHubUsername);
+    getRepositories(user.gitHubUsername, getComms);
+}
+
+function getComms(repos) {
+    console.log('Repos found: ', repos);
+    getCommits(repos, displayCommits);
+}
+
+function displayCommits(commits) {
+    console.log('Commits found: ', commits);
+}
 
 function getUser(id, callback) {
     setTimeout(() => {
@@ -16,7 +25,14 @@ function getUser(id, callback) {
 
 function getRepositories(username, callback) {
     setTimeout(() => {
-        console.log('Reading repositories from database for user: ', username);
+        console.log('Reading repositories from database...');
         callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+}
+
+function getCommits(repoID, callback) {
+    setTimeout(() => {
+        console.log('Reading commits from database...');
+        callback(['commit1', 'commit2', 'commit3']);
     }, 2000);
 }
