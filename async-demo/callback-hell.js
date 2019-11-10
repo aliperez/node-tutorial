117639155c5c71c98e22c5b174d43cdd361a59d7
ALICIA <alicia.perez@utexas.edu>
2019-11-10 09:10:03 -0800

    async and await and error

--- async-demo/callback-hell.js
+++ async-demo/callback-hell.js
@@ -8,11 +8,25 @@ console.log('Before');
 //   })
 // });
 
-getUser(1)
-    .then(user => getRepositories(user.gitHubUsername))
-    .then(repos => getCommits(repos))
-    .then(commits => console.log('Commits: ', commits))
-    .catch(err => console.log('Error', err.message));
+// getUser(1)
+//     .then(user => getRepositories(user.gitHubUsername))
+//     .then(repos => getCommits(repos))
+//     .then(commits => console.log('Commits: ', commits))
+//     .catch(err => console.log('Error', err.message));
+
+// Async and Await approach
+async function displayCommits() {
+    try {
+        const user = await getUser(1);
+        const repos = await getRepositories(user.gitHubUsername);
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    }
+    catch (err) {
+        console.log('Error', err.message);
+    }
+}
+displayCommits();
 
 console.log('After');
 
@@ -29,7 +43,8 @@ function getRepositories(username) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
           console.log('Calling GitHub API...');
-          resolve(['repo1', 'repo2', 'repo3']);
+        //   resolve(['repo1', 'repo2', 'repo3']);
+        reject(new Error('Could not get the repos...'));
         }, 2000);
     });
 }
