867c74febcae024ba0d124dcbf82f4f8631deca9
ALICIA <alicia.perez@utexas.edu>
2019-11-10 14:18:01 -0800

    adding documents to mongo

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/playground', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
@@ -12,3 +15,20 @@ const courseSchema = new mongoose.Schema({
     isPublished: Boolean
 });
 
+// pascal naming convention bc Course is a class not an object
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    // camel case bc course is an object 
+    const course = new Course({
+        name: 'Angular Course',
+        author: 'Mosh',
+        tags: ['angular', 'frontend'],
+        isPublished: true
+    });
+    
+    const result = await course.save();
+    console.log(result);
+}
+
+createCourse();
