32e00bcca234ff2c54e4eea2126d9de5a2eb7aa6
ALICIA <alicia.perez@utexas.edu>
2019-11-18 14:00:38 -0800

    custom validation

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -24,7 +24,15 @@ const courseSchema = new mongoose.Schema({
         enum: ['web', 'mobile', 'network']
     },
     author: String,
-    tags: [ String ],
+    tags: {
+        type: Array,
+        validate: {
+            validator: function(v) {
+                return v && v.length > 0;
+            },
+            message: 'A course should have at least one tag.'
+        }
+    },
     date: { type: Date, default: Date.now },
     isPublished: Boolean,
     price: {
@@ -43,9 +51,9 @@ async function createCourse() {
     // camel case bc course is an object 
     const course = new Course({
         name: 'Fourth Course',
-        category: '-',
+        category: 'web',
         author: 'Mosh',
-        tags: ['fourth', '4th'],
+        tags: null,
         isPublished: true,
         price: 15
     });
