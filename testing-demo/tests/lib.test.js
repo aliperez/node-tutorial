1a1c9f4516fae112b2542686d2d55dcdab6f545f
ALICIA <alicia.perez@utexas.edu>
2020-03-05 13:32:54 -0800

    testing demo

---
+++ testing-demo/tests/lib.test.js
@@ -0,0 +1,99 @@
+const lib = require('../lib');
+const db = require('../db');
+
+describe('absolute', () => {
+    it('should return a positive number if input is positive', () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
+    
+    it('should return a positive number if input is negative', () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
+    
+    it('should return 0 if input is 0', () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
+});
+
+describe('greet', () => {
+    it('should return the greeting message', () => {
+        const result = lib.greet('Mosh');
+        // Regular expression
+        // expect(result).toMatch(/Mosh/);
+        // Without regular expression
+        expect(result).toContain('Mosh');
+    });
+});
+
+describe('getCurrencies', () => {
+    it('should return supported currencies', () => {
+        const result = lib.getCurrencies();
+
+        // Too general
+        // expect(result).toBeDefined();
+        // expect(result).not.toBeNull();
+
+        // Too specific
+        // expect(result[0]).toBe('USD');
+        // expect(result[1]).toBe('AUD');
+        // expect(result[2]).toBe('EUR');
+        // expect(result.length).toBe(3);
+
+        // Proper way
+        // expect(result).toContain('USD');
+        // expect(result).toContain('AUD');
+        // expect(result).toContain('EUR');
+
+        // Ideal way
+        expect(result).toEqual(expect.arrayContaining(['EUR', 'AUD', 'USD']));
+    });
+});
+
+describe('getProduct', () => {
+    it('should return the product with the given id', () => {
+        const result = lib.getProduct(1);
+        // expect(result).toEqual({ id: 1, price: 10 });
+        // toMatchObject ensures id and price are contained in the object, but it can include more than just these two
+        // expect(result).toMatchObject({ id: 1, price: 10 });
+        expect(result).toHaveProperty('id', 1);
+    });
+});
+
+describe('registerUser', () => {
+    it('should throw if username is falsy', () => {
+        // Null
+        // undefined
+        // Nan
+        // ''
+        // 0
+        // false
+        const args = [null, undefined, NaN, '', 0, false];
+        args.forEach(a => {
+            // using a callback below
+            expect(() => { lib.registerUser(a) }).toThrow();
+        });
+    });
+
+    // Happy path
+    it('should return a user object if valid username is passed', () => {
+        const result = lib.registerUser('mosh');
+        expect(result).toMatchObject({ username: 'mosh' });
+        expect(result.id).toBeGreaterThan(0);
+    });
+});
+
+describe('applyDiscount', () => {
+    it('should apply 10% discount if customer has more than 10 points', () => {
+        db.getCustomerSync = function(customerId) {
+            console.log('Fake reading customer...');
+            return { id: customerId, points: 20 };
+        };
+
+        const order = { customerId: 1, totalPrice: 10 };
+        lib.applyDiscount(order);
+        expect(order.totalPrice).toBe(9); 
+    });
+});
