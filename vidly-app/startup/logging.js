0a0667d6c75768bb17f2e07b590cf651e5e888bb
ALICIA <alicia.perez@utexas.edu>
2020-03-09 14:01:38 -0700

    ready to begin integration testing

--- vidly-app/startup/logging.js
+++ vidly-app/startup/logging.js
@@ -1,5 +1,5 @@
 const winston = require('winston');
-require('winston-mongodb');
+// require('winston-mongodb');
 require('express-async-errors');
 
 module.exports = function() {
@@ -12,10 +12,10 @@ module.exports = function() {
     });
 
     winston.add(new winston.transports.File({ filename: "logfile.log" }));
-    winston.add(new winston.transports.MongoDB({ 
-        db: "mongodb://localhost/vidly",
-        level: 'info'
-    }));
+    // winston.add(new winston.transports.MongoDB({ 
+    //     db: "mongodb://localhost/vidly",
+    //     level: 'info'
+    // }));
 
     // // Pretend this is a failed call to a db or something
     // const p = Promise.reject(new Error('Something failed miserably!'));
